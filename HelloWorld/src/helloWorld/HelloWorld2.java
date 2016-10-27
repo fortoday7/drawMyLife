d2e880cfb2751e5c3f81eb5c873f821d0363434f
JuHyeonTae <jabel123@naver.com>
2016-10-27 13:08:41 +0900

    get yop

--- HelloWorld/src/helloWorld/HelloWorld2.java
+++ HelloWorld/src/helloWorld/HelloWorld2.java
@@ -3,5 +3,6 @@ package helloWorld;
 public class HelloWorld2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Universe i'm bewhy");
+		System.out.println("Get up Hot san!");
 	}
 }
